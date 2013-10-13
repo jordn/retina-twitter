ecb0cb1b4d54100f11d02d112484ca73852fa04e
Jordan Burgess <jordanburgess@gmail.com>
2013-10-13 13:54:00 +0100

    published on google app store

---
+++ README.md
@@ -0,0 +1,13 @@
+Retina-Twitter
+==============
+
+> Replaces twitter profile images with their bigger version to make twitter.com look better on retina displays.
+
+To install go to **https://chrome.google.com/webstore/detail/retina-twitter/hgdfdmoaogekkpncleidmpddpijgeakp**
+
+
+Only currently replaces the 48 x 48px avatar with their 73 x 73px ('bigger') version. Bigger images are available but don't keep to the same square ratio.
+
+*Pull requests very much welcome.*
+
+MIT License.
--- main.js
+++ main.js
@@ -1,43 +1,10 @@
-// var avatars = $('img.avatar');
-// 'avatars: '+ avatars;
-// for (var i = avatars.length - 1; i >= 0; i--) {
-//   i;
-//   // avatars[i];
-// };
-
-// avatars.map( function(avatar) {
-//   console.log('unedited: '+ avatar)
-//   if (avatar.src !== '') {
-//     var src = avatar.src;
-//     if (src) {
-//       src_bigger = src.replace("_normal", "_bigger");
-//       avatar.src = src_bigger;
-//       console.log(avatar);
-//     }
-//   } else {
-//     console.log('no src')
-//   }
-// // })
-// $( "img.avatar" ).hover(function() {
-//   $(this).attr("src", $(this).attr("src").replace("_normal", "_bigger"));
-// });
-
-// $(window).onready(function() {
-//   var avatars = $("img.avatar");
-  
-//   $( "img.avatar" ).hover(function() {
-//     $(this).attr("src", $(this).attr("src").replace("_normal", "_bigger"));
-//   });
-// })
-console.log('hi')
-document.body.style.backgroundColor="red"
-
-
+console.log('Running Retina-Twitter extension')
+// document.body.style.backgroundColor="red"
 function replaceWithBigger() {
   $("img.avatar[src]").each(function() {
     var img = $(this);
-    img.attr('src', img.attr('src').replace("_normal", ""));
-    img.attr('src', img.attr('src').replace("_bigger", ""));
+    // img.attr('src', img.attr('src').replace("_bigger", "")); //Disables as the biggest images do not keep the square ratio
+    img.attr('src', img.attr('src').replace("_normal", "_bigger"));
   });
 }
 
